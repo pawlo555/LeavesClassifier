7f42aea4f0a1c690b73d1a90eeb2d9df6ad34412
pawlo555 <spawel555@outlook.com>
2021-04-11 18:50:38 +0200

    Evaluate model update

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-import os
 
 from dataset_loader import Dataset
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -10,11 +9,6 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-for root, dirs, files in os.walk(r"C:\Studia\Biologiczne\LeavesClassifier\PlantVillage-Dataset\raw\color", topdown=False):
-   for name in dirs:
-      print(os.path.join(name))
 
-
-my_dataset = Dataset(greyscale=False, segmented=False)
 model = tf.keras.models.load_model("models/inception_01")
-# model.evaluate(my_dataset.train_dataset)
+model.evaluate(Dataset().test_dataset)
